56bfb4de72bf8ab9bbb6a7610d325d1f7197bba2
Melissa <melissa.aguiar@engenharia.ufjf.br>
2020-10-11 16:24:44 -0300

    main atualizado

--- trabalho01/trabalho01/src/main.cpp
+++ trabalho01/trabalho01/src/main.cpp
@@ -32,10 +32,6 @@ int main()
     fooVazao(vazao2);
 
     //###################################### CORRENTE E TENSAO ##################################
-    // Sempre que for criar vetor com getDadosSalvos de corrente ou tensao, é preciso enviar dois indices.
-    // Por padrao inicializei os indices com 0 e 1, mas o usuario que escolhe o intervalo na interface.
-    // Estes indices representam o intervalo dos dados a serem utilizados nas funçoes de RMS, potencia, energia, etc
-    // funçao de calculo RMS deve ser adicionada no sensorcorrente .cpp e .h
 
     int indice1 = 0;
     int indice2 = 20;
@@ -47,7 +43,6 @@ int main()
     
     fooCorrente(corrente1);
     fooCorrente(corrente2);
-    //corrente1.getDadosSalvos(indice1, indice2);
     
     SensorCorrente tensao1("C:\\Users\\melis\\Desktop\\GitDesk\\_trabalho01\\trabalho01\\dados\\SensorTensao1.dat", headersSensores);
     SensorCorrente tensao2("C:\\Users\\melis\\Desktop\\GitDesk\\_trabalho01\\trabalho01\\dados\\SensorTensao2.dat", headersSensores);
@@ -55,17 +50,30 @@ int main()
     fooCorrente(tensao1);
     fooCorrente(tensao2);
 
-    // #################################### RMS E POTENCIA APARENTE #########################
+    // #################################### CALCULO DE RMS E POTENCIA APARENTE (FUNCIONANDO) #########################
     double iRMS1 = corrente1.getRMS(indice1, indice2);
     double vRMS1 = tensao1.getRMS(indice1, indice2);
     double potAparente = iRMS1*vRMS1;
 
-    cout << "valor de corrente RMS no horario desejado: " << iRMS1 << endl;
-    cout << "valor de tensao RMS no horario desejado: " << vRMS1 << endl;
+    cout << "valor de corrente RMS no sensor " << corrente1.getNome() << " no horario desejado: " << iRMS1 << endl;
+    cout << "valor de tensao RMS no sensor " << tensao1.getNome() << "  no horario desejado: " << vRMS1 << endl;
     cout << "valor de potencia aparente no horario desejado: " << potAparente << endl;
 
 
-    //################################ INTERFACE RESERVATORIO ###############################
+    //################################ VAZAO E VOLUME (FUNCIONANDO) #############################################
+    double volume1 = vazao1.getVolume(indice2);
+    double volume2 = 1000 * vazao2.getVolume(indice2);
+    double volumeTotal = volume1 + volume2; //confirmar se é pra somar as duas bombas mesmo ou se é pra subtrair a 1 da 2
+     if (volumeTotal>30000) // volume maximo do reservatorio
+        {
+            volumeTotal = 30000;
+        }
+
+    cout << "Vazao na bomba " << vazao1.getNome() << " neste horario: " << vazao1.getDado(indice2) << vazao1.getUnidade() << endl;
+    cout << "Vazao na bomba " << vazao2.getNome() << " neste horario: " << vazao2.getDado(indice2) << vazao2.getUnidade() << endl;
+    cout << "Volume no reservatorio neste horario: " << volumeTotal << " litros." << endl;
+    
+    //################################ INTERFACE RESERVATORIO (CONVERTE A HORA NO INDICE DO DADO DESEJADO) ###############################
     // se nao puder deixar a interface no main, mover ela pro plantaindustrial .cpp e .h
 
     //string time;
@@ -89,19 +97,6 @@ int main()
     // indice 2 = indice; //fazer isso quando o usuario digita um horario só, pq aí o indice 1 ja esta como zero
     // quando o usuario digitar dois horarios, cada um é um dos indices, pra gerar o arquivo com os dados nesse intervalo 
     //cout << indice<< endl;
-
-    //double volume1 = vazao1.getVolume(indice);
-    //double volume2 = 1000 * vazao2.getVolume(indice);
-    //double volumeTotal = volume1 + volume2;
-    // if (volumeTotal>30000) // volume maximo do reservatorio
-    //    {
-    //        volumeTotal = 30000;
-    //    }
-
-    //cout << "Vazao na bomba " << vazao1.getId() << " neste horario: " << vazao1.getDado(indice) << vazao1.getUnidade() << endl;
-    //cout << "Vazao na bomba " << vazao2.getId() << " neste horario: " << vazao2.getDado(indice) << vazao2.getUnidade() << endl;
-    //cout << "Volume no reservatorio neste horario: " << volumeTotal << " litros." << endl;
-    
 
     return 0;
 }
