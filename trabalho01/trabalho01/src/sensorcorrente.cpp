8b6b88d1e9a462f77794e5ee0768012472050a32
Melissa <melissa.aguiar@engenharia.ufjf.br>
2020-10-13 13:19:30 -0300

    backup versão funcionando ok

--- trabalho01/trabalho01/src/sensorcorrente.cpp
+++ trabalho01/trabalho01/src/sensorcorrente.cpp
@@ -195,17 +195,17 @@ double SensorCorrente::getRMS(const int &indice1, const int &indice2)
 
     double x2=0;									// Vari?vel que armazena o valor RMS e que armazena o valor RMS� (x2 = RMS�)
 	double xo;											// Vari?vel que armazena a amostra a ser descartada
-	double *const bufferCalculo = new double [N];		// Vetor que ir? armazenar os dados de 1 ciclo da onda (Aloca��o din�mica)
+	double *const bufferCalculo = new double [this->N];		// Vetor que ir? armazenar os dados de 1 ciclo da onda (Aloca��o din�mica)
 	double *ptr_bufferCalculo = bufferCalculo;			// ponteiro que ser? utilizado para varrer os dados
-	memset(bufferCalculo, 0 , N*sizeof(double));		// Limpa os valores do vetor
+	memset(bufferCalculo, 0 , this->N*sizeof(double));		// Limpa os valores do vetor
 	
 	for (int i = 0; i < indice2;++i)
 	{
 		xo = *ptr_bufferCalculo;						//Ao inserir uma nova amostra no vetor, retira a amostra mais antiga e armazena-a em xo
 		*ptr_bufferCalculo = (this->dadosRMS[i].valor)*(this->dadosRMS[i].valor);					//Armazena o novo valor no vetor (x�)
-		x2 += (*ptr_bufferCalculo - xo)/N;				//Soma a contribui??o
+		x2 += (*ptr_bufferCalculo - xo)/this->N;				//Soma a contribui??o
         
-        if(ptr_bufferCalculo < (bufferCalculo+N-1))    //Enquanto o ponteiro apontar para um endere�o que esteja contido no bloco de mem?ria apontado por bufferCalculo
+        if(ptr_bufferCalculo < (bufferCalculo+this->N-1))    //Enquanto o ponteiro apontar para um endere�o que esteja contido no bloco de mem?ria apontado por bufferCalculo
 		{
 			ptr_bufferCalculo++;					   //Incremente o ponteiro do buffer;
 			
