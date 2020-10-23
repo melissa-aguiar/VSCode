d225c4f44564511e725a7b6009b296cd73855aea
sephiroth6001 <renan.faria1@ice.ufjf.br>
2020-10-23 20:10:59 -0300

    Add files via upload

---
+++ potencia.h
@@ -0,0 +1,10 @@
+#ifndef POTENCIA_H
+#define POTENCIA_H
+#include"sensorcorrente.h"
+
+
+double calcPot( int , int, int, SensorCorrente&, SensorCorrente&);
+double calcEnergia(int,int,int, SensorCorrente&, SensorCorrente&);
+
+
+#endif 
